137a7fa96342f4e4400d831ba14c7a08578d1656
AaronV77 <aaronvaloroso77@gmail.com>
2018-12-01 13:29:45 -0600

    Initial commit

---
+++ pull.sh
@@ -0,0 +1,213 @@
+#!/bin/sh
+
+#/*-------------------------------------------------------------------
+#Author: Aaron Anthony Valoroso
+#Date: November 14th, 2018
+#License: GNU GENERAL PUBLIC LICENSE
+#Email: valoroso99@gmail.com
+#--------------------------------------------------------------------*/
+argument=""
+argument_size=""
+error_switch=0
+test_switch=0
+current_directory=$(pwd)
+ip_address=None
+username=""
+
+storage_location="Documents/storage"
+found_switch="not_found"
+
+# Check the incoming parameters to see if either testing or printing errors
+# - needs to be turned on.
+argument=$1
+while test $# -gt 0; do
+    if [ "$1" = "-error" ]; then
+        error_switch=1;
+    elif [ "$1" = "-test" ]; then
+        test_switch=1;
+    fi
+    shift
+done
+
+# Remove the beginning and end slash of an incoming directory if it has it.
+# - Have to do this because the find name option will spit out a warning
+# - if I do not remove the slash.
+if [ ! -z "$argument" ] || [ "$argument" != "" ]; then
+    if [ "${argument:0:1}" = '/' ]; then
+        argument="${argument:1}"
+    fi
+
+    if [ "${argument: -1}" = '/' ]; then
+        argument=${argument::-1}
+    fi
+else
+    if [ $test_switch -eq 0 ]; then
+        echo "File / Directory could not be found on Client..."
+    else
+        echo "1"
+    fi
+    exit
+fi
+
+# This will be a place to put all the files to transfer from the server.
+if ! [ -d ~/Transfer ]; then
+    mkdir ~/Transfer
+fi
+
+if [ $test_switch -eq 0 ]; then
+    echo "Step-1: Locating File on server."
+fi
+ssh $username@$ip_address -T > ~/Transfer/transfer.txt << EOSSH
+
+    cd ~/Transfer
+
+    array=(\$(find \$HOME/$storage_location -name "$argument"))
+    len=\${#array[*]}
+
+    if [ \$len == 0 ]; then
+        if [ $test_switch -eq 0 ]; then
+            echo "There were no files found with that given name..."
+            echo ""
+        fi
+        echo "1"
+    elif [ \$len -ge 2 ]; then
+        if [ $test_switch -eq 0 ]; then
+            echo "There were more than one file found with that name..."
+            echo ""
+        fi
+        echo "1"
+    else
+        echo \${array[0]}
+        lines=\$(find \${array[0]} | wc -l)
+        echo \$lines
+        echo ""
+    fi
+
+    exit
+EOSSH
+
+# Move to the transfer directory, get the last line in the output file from
+# - the ssh, then remove the last section on the path, and then remove the
+# - output file. I have to get the tail of the file because I couldn't figure
+# - out how to stop the ssh banner showing up in the output file, and I am
+# - removing the last part of the path so that I have the absoute path of where
+# - the file or directory is to download when I do the sftp. 
+cd ~/Transfer
+error=$(tail -1 transfer.txt)
+if [ "$error" == '1' ]; then
+    if [ $test_switch -eq 0 ]; then
+        echo "ERROR: There was an issue retrieving the file."
+        echo "$path_to_download"
+    fi
+    tail -2 transfer.txt
+    rm transfer.txt
+    cd $current_directory
+    if [ -d ~/Transfer ]; then
+        rm -rf ~/Transfer
+    fi
+    exit
+fi
+path_to_download=$(tail -3 transfer.txt | head -1)
+number_of_lines=$(tail -2 transfer.txt | head -1)
+rm transfer.txt
+if [ $test_switch -eq 0 ]; then 
+    echo "Finished."
+fi
+
+# Need to do SFTP because there is no way to transfer files from the server
+# - back to the client. In order for the server to be able to scp files back
+# - to the client, port 22 has to be open. The question is, how is this done
+# - when my laptop is using hotspot through Version, and how about work WIFI?
+# - So, this is the easiest way to do it from the client side. Lastly, I had
+# - to break path up so that when I did the get command, I didn't get the warning
+# - that the name didn't need to have a slash in it..
+if [ $test_switch -eq 0 ]; then
+    echo "Step-2: Pulling File"
+fi
+
+scp -r $username@$ip_address:$path_to_download . > output.txt
+
+if [ $error_switch == 1 ]; then
+    sed -i 's/^/\t\t/' output.txt
+    cat output.txt
+fi
+
+rm output.txt
+
+if [ $test_switch -eq 0 ]; then 
+    echo "Finished."
+fi
+
+number_of_lines_2=$(find $argument | wc -l)
+if [ $number_of_lines != $number_of_lines_2 ]; then
+    if [ $test_switch -eq 0 ]; then 
+        echo "Not everything made it over from the dark side."
+    else
+        echo "1"
+    fi
+    rm -rf *
+    cd $current_directory
+    echo 1
+    if [ -d ~/Transfer ]; then
+        rm -rf ~/Transfer
+    fi
+    exit
+fi
+
+# Try and find the file that is getting added to the client computer to see if 
+# - it is already in the file system. Now I have the starting point in /home, 
+# - because you will get permission warnings if you dig through the root directories.
+# - You can change this but just keep in mind that this search will take a little bit
+# - longer to search through.
+array=(`find /home -name "$argument"`)
+len=${#array[*]}
+
+# If the find command has found the file or directory (once) on the file system then it
+# - has found the one that got scp to the Transfer directory. If the find command
+# - has found the file or directory (twice) on the file system then it has found it in
+# - the Transfer directory and the location that the user is looking to swap out for the 
+# - one on the server. I then do some clean up and get rid of the file, and move it to
+# - the location that the user is looking to replace.
+if [ $len == 1 ]; then
+    if [ $test_switch -eq 0 ]; then 
+        echo "Step-3: Adding File / Directory to local collection..." 
+        echo "Finished."
+    fi
+    mv $argument $current_directory
+elif [ $len == 2 ]; then
+    if [ $test_switch -eq 0 ]; then 
+         echo "Step-3: Replacing File / Directory to collection..."
+         echo "Finished."
+    fi
+    absolute_path=${array[1]%/*}
+    if [ -d ${array[1]} ]; then
+        rm -rf ${array[1]}
+        mv $argument $absolute_path
+    elif [ -f ${array[1]} ]; then
+        rm ${array[1]}
+        mv $argument $absolute_path
+    fi
+else
+    if [ $test_switch -eq 0 ]; then 
+        echo "ERROR: There were more than one file found with that name..."
+    else
+        echo "1"
+    fi
+fi
+rm -rf *
+
+# Put the user back into their current directory that they were working from.
+cd $current_directory
+
+# Clean up our transfer directory from the client.
+if [ -d ~/Transfer ]; then
+    rm -rf ~/Transfer
+fi
+
+if [ $test_switch -eq 1 ]; then 
+    echo 0
+fi
+# Useful URL's:
+#   - https://www.digitalocean.com/community/tutorials/how-to-use-sftp-to-securely-transfer-files-with-a-remote-server
+#   - https://www.tutorialspoint.com/unix/unix-basic-operators.htm
+#   - https://zaiste.net/a_few_ways_to_execute_commands_remotely_using_ssh/
---
+++ push.sh
@@ -0,0 +1,204 @@
+#!/bin/sh
+
+#/*-------------------------------------------------------------------
+#Author: Aaron Anthony Valoroso
+#Date: November 14th, 2018
+#License: GNU GENERAL PUBLIC LICENSE
+#Email: valoroso99@gmail.com
+#--------------------------------------------------------------------*/
+argument=""
+argument_size=""
+error_switch=0
+test_switch=0
+current_directory=$(pwd)
+ip_address=None
+username=""
+
+storage_location="Documents/storage"
+compression="tar -czf"
+decompression="tar -xzf"
+found_switch="not_found"
+
+# Check the incoming parameters to see if either testing or printing errors
+# - needs to be turned on.
+argument=$1
+while test $# -gt 0; do
+    if [ "$1" = "-error" ]; then
+        error_switch=1;
+        compression="tar -czvf"
+        decompression="tar -xzvf"
+    elif [ "$1" = "-test" ]; then
+        test_switch=1;
+    fi
+    shift
+done
+
+# Remove the beginning and end slash of an incoming directory if it has it.
+# - Have to do this because the find name option will spit out a warning
+# - if I do not remove the slash.
+if [ ! -z "$argument" ] || [ "$argument" != "" ]; then
+    if [ "${argument:0:1}" = '/' ]; then
+        argument="${argument:1}"
+    fi
+
+    if [ "${argument: -1}" = '/' ]; then
+        argument=${argument::-1}
+    fi
+else
+    if [ $test_switch -eq 0 ]; then
+        echo "File / Directory could not be found on Client..."
+    else
+        echo "1"
+    fi
+    exit
+fi
+
+# Concatenate the current working directory with the incoming argument to 
+# - get the full absolute path. Then check to make sure that the incoming
+# - argument is either a directory or file, else exit. After that, I get 
+# - the number of files / folders of the argument, and will compress the 
+# - incoming argument and transfer it to the server. If there was an error, 
+# - print all of the output.
+if [ $test_switch -eq 0 ]; then
+    echo "Step-1: Packaging items and Transfering to server."
+fi
+absolute_path="$current_directory/$argument"
+if [ -d $absolute_path ] || [ -f $absolute_path ]; then
+    {
+        argument_size=$(find $argument | wc -l)
+        compression="$compression transfer.tar.gz $argument"
+        eval $compression
+        scp transfer.tar.gz $username@$ip_address:~/Transfer
+        rm transfer.tar.gz
+    } > output.txt
+
+    if [ $error_switch = 1 ]; then
+        sed -i 's/^/\t\t/' output.txt
+        cat output.txt
+    fi
+    rm output.txt
+    if [ $test_switch -eq 0 ]; then
+        echo "Finished."
+    fi
+else 
+    if [ $test_switch -eq 0 ]; then
+        echo "File / Directory could not be found on Client..."
+    else
+        echo "1"
+    fi
+    exit
+fi
+
+# This is the main guts of this script. In the first line I am sshing into 
+# - my server, disabling the banner of ssh, and saving all output to a file to
+# - be printed to terminal later. After that, I am using a heredoc to allow me
+# - to write all the commands in this fashion. If I didn't use a heredoc, I would
+# - not be able to use if statments. Next, I switch to our Transfer folder on the
+# - server to decompress the file that we sent over, remove the packaged file, and 
+# - then take the only file, and save the name to a variable. Next, I want to make 
+# - sure that every file made it to the remote server, else print an error. Next, I 
+# - want to make sure that the file / folder that we sent over is any where on the 
+# - server. If there is no matching items then add, if there is one matching item then 
+# - replace, and if there is more than one item then print error statment. How I
+# - replace items in the server is just remove the item from where it lies, and then
+# - move the new item in its exact same place. Lastly, some minor details to point
+# - out is that all the variables created inside of the heredoc need to have a \ 
+# - in front of them and all outside variables of the heredoc do not. The reason is
+# - to break the parsing from local to remote in the ssh parsing of the heredoc.
+if [ $test_switch -eq 0 ]; then
+    echo "Step-2: Replacing items in server."
+fi
+ssh $username@$ip_address -T > output.txt << EOSSH
+
+    cd ~/Transfer
+    decompression="$decompression transfer.tar.gz -m"
+    eval \$decompression
+    rm transfer.tar.gz
+    argument=\$(ls)
+
+    if [ -d "\$argument" ]; then
+        argument_size_2=\$(find "\$argument/" | wc -l)
+    elif [ -f "\$argument" ]; then
+        argument_size_2=\$(find "\$argument" | wc -l)
+    fi
+    
+    if [ "\$argument_size_2" != "$argument_size" ]; then
+        if [ $test_switch -eq 0 ]; then
+            echo \$argument_size_2
+            echo $argument_size
+            echo "Not everything made it over to the darkside."
+        else
+            echo "1"
+        fi
+        rm -rf *
+        exit
+    fi
+
+    array=(\$(find \$HOME/$storage_location -name "\$argument"))
+    len=\${#array[*]}
+    
+    if [ \$len = 0 ]; then
+        if [ $test_switch -eq 0 ]; then
+            echo "Adding File / Directory to collection..."
+        fi
+        mv \$argument ~/Documents/storage
+    elif [ \$len -ge 2 ]; then
+
+        if [[ -d \${array[0]} ]]; then
+            rm -rf \${array[0]}
+        elif [[ -f \${array[0]} ]]; then
+            rm \${array[0]}
+        fi
+        
+        echo "There is more than one File / Directory that have the same name..."
+        echo "1"
+
+        exit
+    else
+        if [ $test_switch -eq 0 ]; then
+            echo "Replacing File / Directory to collection..."
+        fi
+
+        if [[ -d \${array[0]} ]]; then
+            rm -rf \${array[0]}
+        elif [[ -f \${array[0]} ]]; then
+            rm \${array[0]}
+        fi
+        mv \$argument \${array[0]}
+    fi
+    rm -rf *
+    exit
+EOSSH
+
+# This section of code is to make sure that the user gets good feed back and understands
+# - what is happening in the heredoc. If the error parameter eas passed to the system then
+# - I want to print everything out from the previous section of code, else then I just want
+# - to print the one line from the heredoc that was printed. In the first part of that if 
+# - statment, I am adding a tab to every line in the output file and printing the contents.
+if [ $error_switch = 1 ];
+then
+    sed -i 's/^/\t\t/' output.txt
+    cat output.txt
+else 
+    output=$(tail -1 output.txt)
+    if [ "$output" = "1" ]; then
+        echo "1"
+        exit
+    fi
+
+    if [ $test_switch -eq 0 ]; then
+        echo -e "\t $output"
+    fi
+
+fi
+rm output.txt
+if [ $test_switch -eq 0 ]; then 
+    echo "Finished."
+else
+    echo "0"
+fi
+
+
+# Useful URL's:
+#   - https://www.tutorialspoint.com/unix/unix-basic-operators.htm
+#   - https://zaiste.net/a_few_ways_to_execute_commands_remotely_using_ssh/
